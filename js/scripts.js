b0f28539a70e4866909136eff07a789d188985a1
Zokir <example@gmail.com>
2022-12-24 19:09:35 +0000

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+/*global console*/
+// eslint-disable-line no-console
+
+
+// alert("Hello Zokir");
+// const place = confirm("Are you here?");
+// console.log(place);
+
+// const currentPlace = prompt("Where are you?","");
+// console.log(currentPlace);
+
+// const age = +prompt("How old are you ?");
+// console.log(typeof age);
+
+const favColor = [];
+favColor[0] = prompt("What is your favorite color #1? ");
+favColor[1] = prompt("What is your favorite color #2? ");
+favColor[2] = prompt("What is your favorite color #3? ");
+favColor[3] = prompt("What is your favorite color #4? ");
+console.log(typeof favColor);
+console.log(favColor);
